b4bdbcbe2da5b344a5f491b5995680e1303ff7c7
Олег Луцик <oleglutsik.dev@gmail.com>
2024-04-25 12:09:40 +0300

    Добавьте файлы проекта.

---
+++ ArrayFiltering/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the elements of the original array separated by spaces:");
+        int[] originalArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        Console.WriteLine("Enter the elements of the array for filtering separated by spaces:");
+        int[] arrayForFiltering = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int[] result = FilterArray(originalArray, arrayForFiltering);
+
+        Console.WriteLine("Result after filtering:");
+        foreach (int num in result)
+        {
+            Console.Write(num + " ");
+        }
+    }
+
+    static int[] FilterArray(int[] originalArray, int[] arrayForFiltering)
+    {
+        HashSet<int> filterSet = new HashSet<int>(arrayForFiltering);
+
+        List<int> resultList = new List<int>();
+        foreach (int num in originalArray)
+        {
+            if (!filterSet.Contains(num))
+            {
+                resultList.Add(num);
+            }
+        }
+
+        return resultList.ToArray();
+    }
+}
